fa9a05dfbfcaead97c083ad319f135078a4059d6
TemurMalik <tmaq77@gmail.com>
2026-04-07 15:49:48 +0500

    Backend

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,8 +1,7 @@
 const { Router } = require("express");
 const users = Router();
 
-const { postRegister } = require("../controllers/users.controller");
-const { getUsers } = require("../controllers/users.controller");
+const { postRegister, getUsers } = require("../controllers/users.controller");
 
 users.post("/register", postRegister);
 users.get("/all", getUsers);
